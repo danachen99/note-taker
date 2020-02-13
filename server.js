dcc02e355e5db2b1c6cac6c20b5b71b300371299
dc <chendana95@gmail.com>
2020-02-13 18:35:09 -0500

    added more routes and comments for each route

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const db = require("./db/db.json");
+const fs = require("fs");
 
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,14 +13,52 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/index", function(req, res) {
+// index route
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", function(req, res) {
+// route for writing/saving/deleting notes
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.listen(PORT, function() {
+// GET notes API
+app.get("/api/notes", (req, res) => {
+    return res.json(db);
+});
+
+// GET note API by ID? 
+app.get("/api/notes/:id", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// POST ... write to db.json
+app.post("/api/notes", (req, res) => {
+    db.push(req.body);
+    fs.writeFile("./db/db.json", JSON.stringify(db), err => {
+        if (err) throw err;
+    });
+    res.send();
+});
+
+app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
 });
+
+
+
+/*
+
+app.delete(`/api/notes/:id`, function(req, res) {
+  
+  const chosen = req.params.id;
+  const index = db.findIndex( note => note.id === chosen);
+  db.splice(index, 1);
+  fs.writeFile("develop/db/db.json", JSON.stringify(db), err => {
+    if (err) throw err;
+  });
+  res.send();
+});
+
+ */
